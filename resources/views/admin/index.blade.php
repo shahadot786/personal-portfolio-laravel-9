e643dfc0702dc9b4dd963f6b9ae6789bb649ab6f
shrCreation <shrhossain786@gmail.com>
2023-01-15 23:06:33 +0600

    fix admin index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,6 @@
 @section('admin')
     <div class="page-content bg-dark">
         <div class="container-fluid">
-
             <!-- start page title -->
             <div class="row">
                 <div class="col-12">
@@ -11,10 +10,14 @@
                         @php
                             $id = Auth::user()->id;
                             $adminData = App\Models\User::find($id);
+                            $totalSkills = App\Models\Skills::count();
+                            $totalTrainings = App\Models\Training::count();
+                            $totalEducations = App\Models\Education::count();
                         @endphp
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="{{ route('admin.profile') }}">{{ $adminData->username }}</a></li>
+                                <li class="breadcrumb-item"><a
+                                        href="{{ route('admin.profile') }}">{{ $adminData->username }}</a></li>
                                 <li class="breadcrumb-item active">Dashboard</li>
                             </ol>
                         </div>
@@ -30,15 +33,14 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="flex-grow-1">
-                                    <p class="text-truncate font-size-14 mb-2">Total Sales</p>
-                                    <h4 class="mb-2">1452</h4>
+                                    <p class="text-truncate font-size-14 mb-2">Total Skills</p>
+                                    <h2 class="mb-2">{{ $totalSkills }}</h2>
                                     <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i
-                                                class="ri-arrow-right-up-line me-1 align-middle"></i>9.23%</span>from
-                                        previous period</p>
+                                                class="me-1 align-middle"></i></span></p>
                                 </div>
                                 <div class="avatar-sm">
                                     <span class="avatar-title bg-light text-primary rounded-3">
-                                        <i class="ri-shopping-cart-2-line font-size-24"></i>
+                                        <i class="ri-shield-cross-fill font-size-24"></i>
                                     </span>
                                 </div>
                             </div>
@@ -50,15 +52,14 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="flex-grow-1">
-                                    <p class="text-truncate font-size-14 mb-2">New Orders</p>
-                                    <h4 class="mb-2">938</h4>
+                                    <p class="text-truncate font-size-14 mb-2">Total Trainings</p>
+                                    <h2 class="mb-2">{{ $totalTrainings }}</h2>
                                     <p class="text-muted mb-0"><span class="text-danger fw-bold font-size-12 me-2"><i
-                                                class="ri-arrow-right-down-line me-1 align-middle"></i>1.09%</span>from
-                                        previous period</p>
+                                                class="me-1 align-middle"></i></span></p>
                                 </div>
                                 <div class="avatar-sm">
                                     <span class="avatar-title bg-light text-success rounded-3">
-                                        <i class="mdi mdi-currency-usd font-size-24"></i>
+                                        <i class="ri-briefcase-2-line font-size-24"></i>
                                     </span>
                                 </div>
                             </div>
@@ -70,15 +71,14 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="flex-grow-1">
-                                    <p class="text-truncate font-size-14 mb-2">New Users</p>
-                                    <h4 class="mb-2">8246</h4>
+                                    <p class="text-truncate font-size-14 mb-2">Total Educations</p>
+                                    <h2 class="mb-2">{{ $totalEducations }}</h2>
                                     <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i
-                                                class="ri-arrow-right-up-line me-1 align-middle"></i>16.2%</span>from
-                                        previous period</p>
+                                                class="me-1 align-middle"></i></span></p>
                                 </div>
                                 <div class="avatar-sm">
                                     <span class="avatar-title bg-light text-primary rounded-3">
-                                        <i class="ri-user-3-line font-size-24"></i>
+                                        <i class="ri-archive-drawer-fill font-size-24"></i>
                                     </span>
                                 </div>
                             </div>
@@ -90,15 +90,14 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="flex-grow-1">
-                                    <p class="text-truncate font-size-14 mb-2">Unique Visitors</p>
-                                    <h4 class="mb-2">29670</h4>
+                                    <p class="text-truncate font-size-14 mb-2">Total Blogs</p>
+                                    <h2 class="mb-2">25</h2>
                                     <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i
-                                                class="ri-arrow-right-up-line me-1 align-middle"></i>11.7%</span>from
-                                        previous period</p>
+                                                class="me-1 align-middle"></i></span></p>
                                 </div>
                                 <div class="avatar-sm">
                                     <span class="avatar-title bg-light text-success rounded-3">
-                                        <i class="mdi mdi-currency-btc font-size-24"></i>
+                                        <i class="ri-contacts-book-2-fill font-size-24"></i>
                                     </span>
                                 </div>
                             </div>
@@ -121,7 +120,7 @@
 
                                 </div>
 
-                                <h4 class="card-title mb-4">Latest Transactions</h4>
+                                <h4 class="card-title mb-4">Latest Blogs</h4>
 
                                 <div class="table-responsive">
                                     <table class="table-centered table-hover table-nowrap mb-0 table align-middle">
@@ -153,120 +152,6 @@
                                                     04 Apr, 2021
                                                 </td>
                                                 <td>$42,450</td>
-                                            </tr>
-                                            <!-- end -->
-                                            <tr>
-                                                <td>
-                                                    <h6 class="mb-0">Alex Adams</h6>
-                                                </td>
-                                                <td>Python Developer</td>
-                                                <td>
-                                                    <div class="font-size-13"><i
-                                                            class="ri-checkbox-blank-circle-fill font-size-10 text-warning me-2 align-middle"></i>Deactive
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    28
-                                                </td>
-                                                <td>
-                                                    01 Aug, 2021
-                                                </td>
-                                                <td>$25,060</td>
-                                            </tr>
-                                            <!-- end -->
-                                            <tr>
-                                                <td>
-                                                    <h6 class="mb-0">Prezy Kelsey</h6>
-                                                </td>
-                                                <td>Senior Javascript Developer</td>
-                                                <td>
-                                                    <div class="font-size-13"><i
-                                                            class="ri-checkbox-blank-circle-fill font-size-10 text-success me-2 align-middle"></i>Active
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    35
-                                                </td>
-                                                <td>
-                                                    15 Jun, 2021
-                                                </td>
-                                                <td>$59,350</td>
-                                            </tr>
-                                            <!-- end -->
-                                            <tr>
-                                                <td>
-                                                    <h6 class="mb-0">Ruhi Fancher</h6>
-                                                </td>
-                                                <td>React Developer</td>
-                                                <td>
-                                                    <div class="font-size-13"><i
-                                                            class="ri-checkbox-blank-circle-fill font-size-10 text-success me-2 align-middle"></i>Active
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    25
-                                                </td>
-                                                <td>
-                                                    01 March, 2021
-                                                </td>
-                                                <td>$23,700</td>
-                                            </tr>
-                                            <!-- end -->
-                                            <tr>
-                                                <td>
-                                                    <h6 class="mb-0">Juliet Pineda</h6>
-                                                </td>
-                                                <td>Senior Web Designer</td>
-                                                <td>
-                                                    <div class="font-size-13"><i
-                                                            class="ri-checkbox-blank-circle-fill font-size-10 text-success me-2 align-middle"></i>Active
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    38
-                                                </td>
-                                                <td>
-                                                    01 Jan, 2021
-                                                </td>
-                                                <td>$69,185</td>
-                                            </tr>
-                                            <!-- end -->
-                                            <tr>
-                                                <td>
-                                                    <h6 class="mb-0">Den Simpson</h6>
-                                                </td>
-                                                <td>Web Designer</td>
-                                                <td>
-                                                    <div class="font-size-13"><i
-                                                            class="ri-checkbox-blank-circle-fill font-size-10 text-warning me-2 align-middle"></i>Deactive
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    21
-                                                </td>
-                                                <td>
-                                                    01 Sep, 2021
-                                                </td>
-                                                <td>$37,845</td>
-                                            </tr>
-                                            <!-- end -->
-                                            <tr>
-                                                <td>
-                                                    <h6 class="mb-0">Mahek Torres</h6>
-                                                </td>
-                                                <td>Senior Laravel Developer</td>
-                                                <td>
-                                                    <div class="font-size-13"><i
-                                                            class="ri-checkbox-blank-circle-fill font-size-10 text-success me-2 align-middle"></i>Active
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    32
-                                                </td>
-                                                <td>
-                                                    20 May, 2021
-                                                </td>
-                                                <td>$55,100</td>
                                             </tr>
                                             <!-- end -->
                                         </tbody><!-- end tbody -->
